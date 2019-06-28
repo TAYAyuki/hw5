2683288cd561695f8b1a33a6e29c50f451097d1b
田屋　侑希 <yuki@Yuki-3.local>
2019-06-28 12:39:00 +0900

    modify pata.html

--- python27-flask/main.py
+++ python27-flask/main.py
@@ -21,18 +21,55 @@ def root():
 # /pata のリクエスト（例えば http://localhost:8080/pata ）をこの関数で処理する。
 # これをパタトクカシーーを処理するようにしています。
 def pata():
-  # とりあえずAとBをつなぐだけで返事を作っていますけど、パタタコカシーーになるように自分で直してください！
-  #pata = request.args.get('a', '') + request.args.get('b', '')
+  # とりあえずAとBをつなぐだけで返事を作っていますけど、パタトクカシーーになるように自分で直してください！
   patoka = request.args.get('a','')
   takushi = request.args.get('b','')
-  for i, pa in enumerate(patoka):
-    pata += pa
-    pata += takushi[i]
+  patatoku = ''
+  if len(patoka) == len(takushi):
+    for i, pa in enumerate(patoka):
+      patatoku += pa
+      patatoku += takushi[i]
+
+  elif len(patoka) > len(takushi):
+    dif = len(patoka)-len(takushi)
+    for i, ta in enumerate(takushi):
+      patatoku += patoka[i]
+      patatoku += ta
+    for i in range(dif):
+      i += len(takushi)
+      patatoku += patoka[i]
+
+  elif len(patoka) < len(takushi):
+    dif = len(takushi) - len(patoka)
+    for i ,pa in enumerate(patoka):
+      patatoku += pa
+      patatoku += takushi[i]
+    for i in range(dif):
+      i += len(patoka)
+      patatoku += takushi[i]
   # pata.htmlのテンプレートの内容を埋め込んで、返事を返す。
-  return render_template('pata.html', pata=pata)
+  return render_template('pata.html', pata=patatoku)
 
 @app.route('/norikae')
 # /norikae のリクエスト（例えば http://localhost:8080/norikae ）をこの関数で処理する。
 # ここで乗り換え案内をするように編集してください。
-def norikae(self):
-  return render_template('norikae.html', network=network)
+def DFS(que,to_station):
+  
+
+def norikae():
+  from_station = request.args.get('from')
+  to_station = request.args.get('to')
+  route = []  
+  que = []
+  for i in range(len(network)):
+    if from_station in network[i]["Stations"]:
+      route.append(from_station)
+      lis = network[i]["Stations"]
+      if to_station in lis:
+        route.append(to_station)
+        break
+      else:
+        que.append(lis)
+        DFS(que,to_station)
+  return render_template('norikae.html', network=route)
+
